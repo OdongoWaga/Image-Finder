c929474728c9614b6b58a593e213eb2e09cd945b
OdongoWaga <39854144+OdongoWaga@users.noreply.github.com>
2018-12-23 16:12:55 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import NavBar from './components/navbar/NavBar';
+import Search from './components/search/Search';
+
+
+ 
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider>
+        <NavBar />
+        <Search />
+      </MuiThemeProvider>
+      
+    );
+  }
+}
+
+export default App;
